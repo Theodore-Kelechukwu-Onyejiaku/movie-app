712d74902570d13c708e196be4a0fcd275f77bdc
Theodore Kelechukwu Onyejiaku <theodoreonyejiaku@gmail.com>
2022-08-16 00:04:38 +0100

    add manifest to create a PWA

--- app.js
+++ app.js
@@ -18,29 +18,29 @@ app.use(express.static("./public"))
  *  GET REQUESTS
  * 
  */
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/result", (req, res)=>{
+app.get("/result", (req, res) => {
 
     //wow req.query is used to pick out the value of any input using GET method
     let query = req.query.name
-    
-    request("https://api.themoviedb.org/3/search/movie?api_key="+process.env.MOVIE_DB_KEY+"&query="+query, (err, response, body)=>{
-        if(err){
+
+    request("https://api.themoviedb.org/3/search/movie?api_key=" + process.env.MOVIE_DB_KEY + "&query=" + query, (err, response, body) => {
+        if (err) {
             console.log(err);
-            res.render("error", {searchQuery: query})
-        }else{
+            res.render("error", { searchQuery: query })
+        } else {
             let data = JSON.parse(body);
-            res.render("result", {data : data, searchQuery : query})
-          }
+            res.render("result", { data: data, searchQuery: query })
+        }
     })
 })
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log("Server running successfuly");
 })
